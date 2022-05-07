1f1063d4c8c7436a6ee6d87e094c53a8a084f018
Montanaro02 <100145766+Montanaro02@users.noreply.github.com>
2022-05-07 18:28:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int dim;
+    cin >> dim;
+    float insieme[dim];
+    for(int i=0; i<dim; i++){
+        cin >> insieme[i];
+    }
+    cout<<maggiore(insieme, dim);
+    return 0;
 }
