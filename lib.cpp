3edbbf3ad06e32601867877de26541c2cca88815
Montanaro02 <100145766+Montanaro02@users.noreply.github.com>
2022-05-07 18:28:21 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
 
+#include "lib.h"
+
+float maggiore (float insieme [], int dim){
+    float maggiore=insieme[0];
+    for (int i=1; i<dim;i++){
+        if (maggiore<insieme [i]) {
+            maggiore=insieme [i];
+        }
+    } return maggiore;
+}
